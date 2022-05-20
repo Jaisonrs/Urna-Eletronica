c43a046ea42d24834597cf3de5dd5a41a1dcfda4
Jaison da Rosa <jaisondarosa_bn@hotmail.com>
2022-05-20 11:27:46 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+Criando uma urna eletrônica utilizando as seguintes tecnologias:
+
+- HTML5
+- CSS3
+- JavaScript
+
+Projeto feito com intenção de por em pratica estudos realizados sobre as tecnologias utilizadas em desenvolvimento FrontEnd
---
+++ etapas.js
@@ -0,0 +1,46 @@
+let etapas = [
+  {
+    titulo: 'VEREADOR',
+    numeros: 5,
+    candidatos: [
+      {
+        numero: 38111,
+        nome: 'Shanks',
+        partido: 'ABC',
+        fotos: [{ url: 'shanks.jpg', legenda: 'Vereador' }],
+      },
+      {
+        numero: 77222,
+        nome: 'Brook',
+        partido: 'DEFG',
+        fotos: [{ url: 'brook.png', legenda: 'Vereador' }],
+      },
+    ],
+  },
+  {
+    titulo: 'PREFEITO',
+    numeros: 2,
+    candidatos: [
+      {
+        numero: 99,
+        nome: 'Monkey D. Luffy',
+        partido: 'CHAPEU',
+        vice: 'Mon',
+        fotos: [
+          { url: 'luffy.png', legenda: 'Prefeito' },
+          { url: 'zoro.jpg', legenda: 'Vice-Prefeito', small: true },
+        ],
+      },
+      {
+        numero: 84,
+        nome: 'Sanji',
+        partido: 'ESPADA',
+        vice: 'San',
+        fotos: [
+          { url: 'sanji.png', legenda: 'Prefeito' },
+          { url: 'chinjao.jpg', legenda: 'Vice-Prefeito', small: true },
+        ],
+      },
+    ],
+  },
+];
